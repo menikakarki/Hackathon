78cf179616c178abdd5a11860fbc00731194beb3
Deepak Gupta <82931946+Deepak-0409@users.noreply.github.com>
2021-06-27 11:00:21 +0530

    Updated for hosting.
    Changes in the middleware section = 'whitenoise.middleware.WhiteNoiseMiddleware',
    
    DEBUG = False    --> default was True
    
    ALLOWED_HOSTS = ['127.0.0.1']   --> default was  ALLOWED_HOSTS = [ ]

--- friendlybug/settings.py
+++ friendlybug/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mh3wyncckx^)s)4m$pbn@yn2gp^r7v_ip@o3j11i%2yt7_5j&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
